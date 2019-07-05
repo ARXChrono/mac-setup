dc6ed15e0335be7ff7c64d0a52b97adf7052b3e7
ARXChrono <ChronoXP@hotmail.com>
2019-07-05 22:39:44 +0800

    add vscode scripts

--- install.sh
+++ install.sh
@@ -44,11 +44,40 @@ brew cask install slack
 brew cask install spotify
 brew cask install notion
 
-# TODO: vscode plugins, themes, and import config
+# VSCode - Theme, Extensions, todo: export config
+
+code --install-extension monokai.theme-monokai-pro-vscode
+code --install-extension pkief.material-icon-theme
+
+code --install-extension mgmcdermott.vscode-language-babel
+code --install-extension apollographql.vscode-apollo
+code --install-extension Prisma.vscode-graphql
+code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension burkeholland.react-food-truck
+code --install-extension burkeholland.simple-react-snippets
+code --install-extension capaj.vscode-exports-autocomplete
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension eamodio.gitlens
+code --install-extension dzannotti.vscode-babel-coloring
+code --install-extension eg2.vscode-npm-script
+code --install-extension eriklynd.json-tools
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension wix.vscode-import-cost
+
 
 # TODO: find automation for github ssh setup, will have to do below manually for now.
 # ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
 # eval "$(ssh-agent -s)"
 # pbcopy < ~/.ssh/id_rsa.pub
+# git config --global user.name "ARXChrono" 
+# git config --global user.email "email@example.com"
 
-echo "Done ✨"
+echo "\033[32m Done ✨"
