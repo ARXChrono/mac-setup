41d8f4c57a58359473fddf7ce466539d10472086
ARXChrono <ChronoXP@hotmail.com>
2019-07-06 21:05:06 +0800

    fix empty space

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Import Scripts
-echo -e "\033[32m Configuring Shaun's Mac Setup 🚚"
+echo -e "\033[32mConfiguring Shaun's Mac Setup 🚚"
 
 bash <(curl -s https://raw.githubusercontent.com/ARXChrono/mac-setup/master/scripts/macConfig.sh)
 bash <(curl -s https://raw.githubusercontent.com/ARXChrono/mac-setup/master/scripts/install.sh)
 
-echo -e "\033[32m Shaun's Mac Setup Done ✨"
+echo -e "\033[32mShaun's Mac Setup Done ✨"
