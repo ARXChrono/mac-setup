dbdcec414f18a4825e812987a64d207b970e57b0
ARXChrono <ChronoXP@hotmail.com>
2019-07-06 17:45:35 +0800

    add note zsh config

--- configs/zsh.sh
+++ configs/zsh.sh
@@ -17,4 +17,6 @@ antigen theme robbyrussell
 # Tell Antigen that you're done.
 antigen apply
 
-echo -e "\033[32m ✅ zsh configured completed"
+echo -e "\033[32m✅ zsh configured completed"
+
+# Edit this one doesn't seem to work right when imported but works when running directly.
