587d34e60ad0726387bb7b911cc207343dbed482
ARXChrono <ChronoXP@hotmail.com>
2019-07-07 22:21:12 +0800

    add vscode plugins, figma & bannercommentsplus

--- scripts/install.sh
+++ scripts/install.sh
@@ -100,6 +100,8 @@ code --install-extension mgmcdermott.vscode-language-babel
 code --install-extension Prisma.vscode-graphql
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension wix.vscode-import-cost
+code --install-extension lunarlimbo.banner-comments-plus
+code --install-extension idered.figma
 
 # TODO: find automation for github ssh setup, will have to do below manually for now.
 # ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
