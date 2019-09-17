61d1f7af64ca4706c0d908b6512bf6699fce8554
Shaun <chronoxp@hotmail.com>
2019-09-17 14:05:25 +0800

    add yarn alias

--- configs/zsh.sh
+++ configs/zsh.sh
@@ -10,7 +10,21 @@ echo "✅ add zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 echo "source ${(q-)PWD}/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
-echo "✅ add gs alias"
+echo "✅ add zsh config alias"
+echo 'alias zshconfig="code ~/.zshrc"' >> ${ZDOTDIR:-$HOME}/.zshrc
+
+echo "✅ add git alias"
 echo "alias gs='git status'" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+echo "✅ add yarn alias"
+echo 'alias ys="yarn start"' >> ${ZDOTDIR:-$HOME}/.zshrc
+echo 'alias yd="yarn dev"' >> ${ZDOTDIR:-$HOME}/.zshrc
+echo 'alias yl="yarn lint"' >> ${ZDOTDIR:-$HOME}/.zshrc
+echo 'alias yys="yarn && yarn start"' >> ${ZDOTDIR:-$HOME}/.zshrc
+echo 'alias yc="yarn clean"' >> ${ZDOTDIR:-$HOME}/.zshrc
+echo 'alias yb="yarn build"' >> ${ZDOTDIR:-$HOME}/.zshrc
+echo 'alias ysv="yarn serve"' >> ${ZDOTDIR:-$HOME}/.zshrc
+echo 'alias ybs="yarn build && yarn serve"' >> ${ZDOTDIR:-$HOME}/.zshrc
+echo 'alias yui="yarn upgrade-interactive --latest"' >> ${ZDOTDIR:-$HOME}/.zshrc
 
 echo -e "\033[32m✅ zsh configured completed"
