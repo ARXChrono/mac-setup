890fdbaedd7d3670c56fc793c43caae5186b6f01
ARXChrono <chronoxp@hotmail.com>
2023-07-03 17:01:45 +0800

    remove comments

--- configs/zsh.sh
+++ configs/zsh.sh
@@ -2,21 +2,15 @@
 
 echo "🛠 Configuring zshrc"
 
-echo "✅ add zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 echo "source ${(q-)PWD}/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
-echo "✅ add zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 echo "source ${(q-)PWD}/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
-echo "✅ add zsh config alias"
 echo 'alias zshconfig="code ~/.zshrc"' >> ${ZDOTDIR:-$HOME}/.zshrc
-
-echo "✅ add git alias"
 echo "alias gs='git status -sb'" >> ${ZDOTDIR:-$HOME}/.zshrc
 
-echo "✅ add yarn alias"
 echo 'alias ys="yarn start"' >> ${ZDOTDIR:-$HOME}/.zshrc
 echo 'alias yd="yarn dev"' >> ${ZDOTDIR:-$HOME}/.zshrc
 echo 'alias yl="yarn lint"' >> ${ZDOTDIR:-$HOME}/.zshrc
