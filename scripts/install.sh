5b604d85105eef59219ce90d06f4826c17644698
ARXChrono <chronoxp@hotmail.com>
2023-07-03 16:51:05 +0800

    clean up vscode plugins

--- scripts/install.sh
+++ scripts/install.sh
@@ -51,20 +51,15 @@ code --install-extension dracula-theme.theme-dracula
 code --install-extension pkief.material-icon-theme
 
 ## Extensions
-code --install-extension aliariff.auto-add-brackets
-code --install-extension capaj.vscode-exports-autocomplete
-code --install-extension christian-kohler.npm-intellisense
-code --install-extension christian-kohler.path-intellisense
-code --install-extension CoenraadS.bracket-pair-colorizer
-code --install-extension dbaeumer.vscode-eslint
+code --install-extension github.copilot
 code --install-extension eamodio.gitlens
-code --install-extension eg2.vscode-npm-script
-code --install-extension eriklynd.json-tools
 code --install-extension esbenp.prettier-vscode
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension mgmcdermott.vscode-language-babel
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension aliariff.auto-add-brackets
 code --install-extension formulahendry.auto-close-tag
 code --install-extension formulahendry.auto-rename-tag
-code --install-extension mgmcdermott.vscode-language-babel
-code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension wix.vscode-import-cost
 code --install-extension shyykoserhiy.vscode-spotify
 
